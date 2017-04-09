d78731f073c3214d447780f1b5c3fa7c992a5319
mohit saran <mohitsaran07@gmail.com>
2017-04-09 16:50:47 +0530

    fix in CSV formate

--- crawlers/spiders/quotes_spider.py
+++ crawlers/spiders/quotes_spider.py
@@ -1,5 +1,10 @@
 import scrapy
 
+class Item(scrapy.Item):
+    URL = scrapy.Field()
+    Price = scrapy.Field()
+    ProductDetails = scrapy.Field()
+    
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
@@ -8,12 +13,12 @@ class QuotesSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        for quote in response.css('ul.ProductList'):
-            yield {
-                'price': quote.css('li div.ProductDetails em ::text').extract(),
-                'url': quote.css('div.ProductDetails a::attr(href)').extract(),
-                'ProductDetails': quote.css('li div.ProductDetails a::text').extract(),
-            }
+        for quote in response.css('ul.ProductList li'):
+                item=Item()
+                item['URL'] = quote.css('div.ProductDetails a::attr(href)').extract()[0],
+                item['Price'] = quote.css('li div.ProductDetails em ::text').extract()[0],
+                item['ProductDetails'] = quote.css('div.ProductDetails a::text').extract()[0],
+                yield item
 
         next_page = response.css('ul.PagingList a::attr(href)').extract_first()
         if next_page is not None:
