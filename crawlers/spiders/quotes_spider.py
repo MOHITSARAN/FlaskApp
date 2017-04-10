a63ef831f92926b501c3c25c50c1859afb0af89d
mohit saran <mohitsaran07@gmail.com>
2017-04-10 17:03:25 +0530

    fix in crawlers api

--- crawlers/spiders/quotes_spider.py
+++ crawlers/spiders/quotes_spider.py
@@ -4,23 +4,30 @@ class Item(scrapy.Item):
     URL = scrapy.Field()
     Price = scrapy.Field()
     ProductDetails = scrapy.Field()
+    Description = scrapy.Field()
     
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
     start_urls = [
-        'https://www.liveyoursport.com/squash/'
+        'https://www.liveyoursport.com/squash'
     ]
 
     def parse(self, response):
-        for quote in response.css('ul.ProductList li'):
-                item=Item()
-                item['URL'] = quote.css('div.ProductDetails a::attr(href)').extract()[0],
-                item['Price'] = quote.css('li div.ProductDetails em ::text').extract()[0],
-                item['ProductDetails'] = quote.css('div.ProductDetails a::text').extract()[0],
-                yield item
+        for href in response.css('div.ProductDetails a::attr(href)'):
+            url = response.urljoin(href.extract())
+            print url
+            yield scrapy.Request(url, callback=self.parse_item)
 
-        next_page = response.css('ul.PagingList a::attr(href)').extract_first()
-        if next_page is not None:
+    def parse_item(self, response):
+        item=Item()
+        item['URL'] = response.css('div.ProductDetails a::attr(href)').extract()[0],
+        item['Description'] = response.css('div.prod-short-desc ::text').extract()[0],
+        item['Price'] = response.css('li div.ProductDetails em ::text').extract()[0],
+        item['ProductDetails'] = response.css('div.ProductDetails a::text').extract()[0],
+        yield item
+
+        next_page = response.css('ul.PagingList a::attr(href)').extract()
+        if next_page:
             next_page = response.urljoin(next_page)
             yield scrapy.Request(next_page, callback=self.parse)
